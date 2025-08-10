8db4d7d2e371f9b2e4fa2e0b0cff246e2a362d79
jiji465 <ryannferreira1@gmail.com>
2025-08-09 21:54:34 -0300

    docs: adicionar config.example.js (preparar credenciais Firebase)

---
+++ config.example.js
@@ -0,0 +1,13 @@
+﻿// Copie este arquivo para config.js e preencha com suas credenciais do Firebase Web App
+// Como obter: Console Firebase > Configurações do projeto > Seus apps > CDN
+window.__app_id = 'calen';
+window.__firebase_config = JSON.stringify({
+  apiKey: 'SUA_API_KEY',
+  authDomain: 'SEU_PROJECT_ID.firebaseapp.com',
+  projectId: 'SEU_PROJECT_ID',
+  storageBucket: 'SEU_PROJECT_ID.appspot.com',
+  messagingSenderId: 'SEU_SENDER_ID',
+  appId: 'SEU_APP_ID'
+});
+// Opcional: se tiver um token de autenticação customizada
+// window.__initial_auth_token = 'TOKEN_JWT_CUSTOM';
