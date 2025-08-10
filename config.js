37c7114a422e45d4f3de2cb9dc75829be0d164a9
jiji465 <122660528+jiji465@users.noreply.github.com>
2025-08-09 22:13:43 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+<script>
+window.__app_id = 'calen';
+window.__firebase_config = JSON.stringify({
+  apiKey: 'SUA_API_KEY',
+  authDomain: 'SEU_PROJECT_ID.firebaseapp.com',
+  projectId: 'SEU_PROJECT_ID',
+  storageBucket: 'SEU_PROJECT_ID.appspot.com',
+  messagingSenderId: 'SEU_SENDER_ID',
+  appId: 'SEU_APP_ID'
+});
+</script>
